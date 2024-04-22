179cbf077e7db8f7c95468a83fde5b0d3a864e7e
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-04-22 18:56:54 -0300

    add animaciões na home, tentativas de correções no responsivo do header

--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -1,11 +1,14 @@
-import  ScrollReveal from 'scrollreveal';
 
-ScrollReveal().reveal('.box-director', {
-  origin: 'left',  // A origem da animação (top, bottom, left, right)
-  distance: '100px', // A distância do elemento em relação à origem
-  duration: 1000, // A duração da animação em milissegundos
-  delay: 0, // O atraso antes que a animação comece
-  easing: 'ease', // A função de easing (suavização) da animação
-  reset: false, // Se a animação deve ser repetida quando o elemento sair e entrar na tela novamente
-  mobile: true // Se a animação deve ser ativada em dispositivos móveis
-});
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show')
+      } else {
+        entry.target.classList.remove('show')
+      }
+    })
+})
+
+const slideleft = document.querySelectorAll(".slider-left")
+
+slideleft.forEach((element) => myObserver.observe(element))
